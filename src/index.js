98e0d12c100e1e53d699ff963431d2a2d7b57894
jsusmachaca <falcorgd@gmail.com>
2024-03-30 14:46:35 -0500

    init testing

--- src/index.js
+++ src/index.js
@@ -34,3 +34,6 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`\x1b[32mserver listening on port ${PORT}\x1b[0m`)
 })
+
+// descoment for testing
+// export default app
---
+++ tests/posts.test.js
@@ -0,0 +1,11 @@
+import app from "../src/index";
+import request from 'supertest'
+
+describe('GET /api/posts/all', () => {
+  test('should respond with a json', async () => {
+    await request(app)
+      .get('/api/posts/all').send()
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })  
+})
