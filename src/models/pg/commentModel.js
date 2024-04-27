62167c5296714f16399c6f8e99d3b4f31139092e
jsusmachaca <falcorgd@gmail.com>
2024-04-26 22:50:35 -0500

    add docstrings on comment Model

--- src/models/pg/commentModel.js
+++ src/models/pg/commentModel.js
@@ -4,6 +4,11 @@ import { dbConnectionPg } from '../../config/config.js'
 const connection = await dbConnectionPg()
 
 export class comment {
+  /**
+   * Obtiene los comentarios de un post por su ID.
+   * @param {number} post_id - El ID del post del cual obtener los comentarios.
+   * @returns {Promise<Array<Object>|{error:string}>} Una promesa que resuelve en un array de objetos representando los comentarios o un objeto de error si ocurre algún problema.
+   */
   static async getComments(post_id) {
     try {
       const { rows } = await connection.query(`
@@ -22,6 +27,15 @@ export class comment {
     }
   }
 
+  /**
+   * Crea un nuevo comentario en la base de datos.
+   * @param {Object} options - Las opciones para crear el comentario.
+   * @param {number} options.user_id - El ID del usuario que realiza el comentario.
+   * @param {number} options.post_id - El ID del post al cual se asocia el comentario.
+   * @param {Object} options.comment - El objeto que contiene el contenido del comentario.
+   * @param {string} options.comment.comment - El contenido del comentario.
+   * @returns {Promise<boolean|{error:string}>} Una promesa que resuelve en verdadero si el comentario se creó exitosamente o un objeto de error si ocurrió algún problema.
+   */
   static async createComment({ user_id, post_id, comment }) {
     try {
       const { rows } = await connection.query(`
@@ -35,7 +49,14 @@ export class comment {
       return { error: error.message }
     }
   }
-
+  
+  /**
+   * Elimina un comentario de la base de datos.
+   * @param {Object} options - Las opciones para eliminar el comentario.
+   * @param {number} options.comment_id - El ID del comentario que se desea eliminar.
+   * @param {number} options.user_id - El ID del usuario que creó el comentario.
+   * @returns {Promise<boolean|{error:string}>} Una promesa que resuelve en verdadero si el comentario se elimina exitosamente o un objeto de error si ocurre algún problema.
+   */
   static async deleteComment({ comment_id, user_id }) {
     try {
       const { rows } = await connection.query(`
